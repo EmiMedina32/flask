f1c6851ae0972d8e5765fe50c2f2804b8a7b34a8
emimedina32 <emilianomedina357@gmail.com>
2024-04-15 13:26:49 -0300

    probar la modificacion de verificar contraseña

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -13,6 +13,7 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        verificar = request.form['verificar']
         db = get_db()
         error = None
 
@@ -20,12 +21,14 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-
+        elif verificar != password:
+            error = 'Las claves no coinciden.'
+        
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (username, password, verificar) VALUES (?, ?, ?)",
+                    (username, generate_password_hash(password), verificar),
                 )
                 db.commit()
             except db.IntegrityError:
